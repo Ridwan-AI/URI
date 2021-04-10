256d92949ebd3d6b5ba828dde70956d97dd1e6dd
Ridwan-AI <ridwan@rnetworks.cf>
2021-04-10 08:51:06 +0600

    Satging

--- 1098.cpp
+++ 1098.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
- 
+#define  ll float 
 using namespace std;
  
 int main() {
- 
-    
+    ll i=0, j=1;
+
+    for (ll counter = 1; counter <= 10; counter++)
+    {
+        cout << "I=" << i << " J=" << j << endl;
+        j++;
+        if (j == 3)
+    }
     printf("\n");
     return 0;
 }
