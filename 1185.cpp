4d3506b5beb919520f5515092d72fb87925f7783
Ridwan-AI <ridwan@rnetworks.cf>
2021-04-21 16:42:00 +0600

    Staged

---
+++ 1185.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll int_fast64_t
+#define ull uint_fast64_t
+#define deci long double
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(1);
+    deci M[12][12];
+    char O;
+    cin >> O;
+    for (ll row = 0; row < 12; row++)
+        for (ll column = 0; column < 12; column++)
+            cin >> M[row][column];
+    deci total = 0;
+
+    for (ll row = 0; row < 12; row++)
+    {
+        for (ll column = 0; column < (11 - row); column++)
+        {
+            total += M[row][column];
+            cout << M[row][column] << " ";
+        }
+        cout << endl;
+    }
+    if (O == 'S')
+        cout << total << endl;
+    else
+        cout << total / 66 << endl;
+    return 0;
+}
