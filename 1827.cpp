3018bb3fdd99112d59161e44bd3f36735dec4bfb
Ridwan-AI <ridwan@rnetworks.cf>
2021-04-29 00:15:57 +0600

    Solved 1827

--- 1827.cpp
+++ 1827.cpp
@@ -9,17 +9,48 @@ int main(void)
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout << fixed << setprecision(0);
-    ll N;
-    deci arra[101][101];
+    ll N, a, b;
+    deci arra[105][105];
     while (1)
     {
+
         cin >> N;
         if (cin.eof())
             return 0;
-        arra[0][0] = 2;
-        arra[0][N - 1] = 3;
-
-
+        a = N;
+        a--;
+        for (ll counter = 0; counter < N; counter++)
+        {
+            for (ll count = 0; count < N; count++)
+            {
+                arra[counter][count] = 0;
+                if (counter == count)
+                {
+                    arra[counter][count] = 2;
+                }
+                else if ((counter + count) == a)
+                {
+                    arra[counter][count] = 3;
+                }
+            }
+        }
+        if ((N % 3) == 0)
+            a = (N / 3);
+        else
+            a = (N / 3);
+        if ((N % 3) == 0)
+            b = (2 * N / 3) - 1;
+        else
+            b = (2 * N / 3);
+        for (ll counter = a; counter <= b; counter++)
+        {
+            for (ll count = a; count <= b; count++)
+            {
+                arra[counter][count] = 1;
+                // cout << counter << " " << count << endl;
+            }
+        }
+        arra[N / 2][N / 2] = 4;
         for (ll counter = 0; counter < N; counter++)
         {
             for (ll count = 0; count < N; count++)
@@ -28,6 +59,7 @@ int main(void)
             }
             cout << endl;
         }
+        cout << endl;
     }
     return 0;
 }
